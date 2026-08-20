994459d9aa5423b332cc29908c3639b47f0d5782
cutep <cutepuppy2ki@gmail.com>
2026-08-20 18:36:22 +0200

    everything

---
+++ case 2/run_ap_1_triangle.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 send.py --script ap_SCRIPTS/triangle.script --robot-ip 192.168.1.100 --loop 1 --out ap_SCRIPTS/data/triangle.csv
---
+++ case 2/run_ap_2_optimized1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 send.py --script ap_SCRIPTS/triangleOptimized1.script --robot-ip 192.168.1.100 --loop 1 --out ap_SCRIPTS/data/triangleOptimized1.csv
